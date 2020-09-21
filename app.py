537e39de5baeb2c371ea82469e4ae7998c97ab36
unknown <sothkimleng007@gmail.com>
2020-09-21 17:17:31 +0700

    Info

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from bottle import route, run
+import os
 
 @route('/')
 def main():
     return "Hellow world"
 
-run(host="localhost", port=9000, debug=True, reloader=True)
+
+if 'DYNO' in os.environ:
+  run(host='0.0.0.0', port=os.environ.get('PORT', 9000))
+else: 
+  run(host='localhost', port=9000, debug=True, reloader=True)
+  
